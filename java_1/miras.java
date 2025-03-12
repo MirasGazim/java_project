00198ea9b76fadf85b19fd09d58c201153e38d98
Газимов Диас <gvzumdias@gmail.com>
2025-03-12 13:28:01 +0500

    nurs is happy

--- java_1/miras.java
+++ java_1/miras.java
@@ -13,13 +13,8 @@ public class miras {
         gazim sss = new gazim(234f, 2000, "black", new byte[] {12, 12, 23});
         System.out.println(sss.getValues());
         gazim rrr = new gazim(345, new byte[] {12,  34, 45});
+        gazim nurs =new gazim(234f, 1233, "white", new byte[] {12, 23, 34, 34});
         
-        int[] num = {1, 1, 2, 1, 4, 4, 1, 4, 3, 3, 4, 2, 3, 2, 3};
-        int unique = 0;
-        for(int i : num)
-            unique ^= i;
-        
-        System.out.println(unique);
     
     }  
 
